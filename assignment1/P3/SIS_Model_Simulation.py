f13c2b5e5afa57b4ec539004530fbe8cdf4f2769
Yan <yanzhanggc@gmail.com>
2019-09-11 19:09:44 -0700

    p4 figure added

--- assignment1/P3/SIS_Model_Simulation.py
+++ assignment1/P3/SIS_Model_Simulation.py
@@ -7,20 +7,41 @@ Created on Wed Sep 11 11:56:04 2019
 """
 from euler_method import *
 from heun_method import *
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+
+plt.style.use('seaborn')
 
 # model parameter
 N = 100
 gamma =  0.25
 
+fig1, axes1 = plt.subplots(nrows=2, ncols=3, sharex=True)
+fig1.set_size_inches(18.5, 10.5)
 
-for beta in [0.03, 0.06, 0.1]:
-    for h in [0.01, 0.5, 2.0]:
-    # the differential equation for I 
+for beta, row in zip([0.03, 0.06, 0.1], range(0, 2)):
+    for h, col in zip([0.01, 0.5, 2.0], range(0, 3)):
+        # the differential equation for I 
         I_dot = lambda t, I: beta*(N - I)*I - gamma*I
+        
+        # set parameters
         I_initial = 10
         a = 0
         b = 50
         
-        t_list, I_list = heun_method(I_dot, I_initial, a, b, h)
-        plt.plot(t_list, I_list)
+        # run integrator and collect data
+        t_list_euler, I_list_euler = euler_method(I_dot, I_initial, a, b, h)
+        t_list_heun, I_list_heun = heun_method(I_dot, I_initial, a, b, h)
+        
+        
+        # begin plot
+        ax = axes1[row, col]
+        ax.plot(t_list_euler, I_list_euler, 
+                label='euler method')
+        ax.plot(t_list_heun, I_list_heun,
+                label='heun method')
+        ax.set_xlabel('time')
+        ax.set_ylabel('number of infectious')
+        ax.set_title('beta = {}, h = {}'.format(beta, h))
+        ax.legend(loc='lower right')
+
+fig1.savefig('p4_figure.png')

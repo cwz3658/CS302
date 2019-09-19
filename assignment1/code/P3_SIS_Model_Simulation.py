333ef2e5f95a638369637cc8cfcdd985e0dfb2d8
LOVGC <yanzhanggc@gmail.com>
2019-09-19 16:16:14 -0400

    code pack

---
+++ assignment1/code/P3_SIS_Model_Simulation.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 11 11:56:04 2019
+
+"""
+from euler_method import *
+from heun_method import *
+from matplotlib import pyplot as plt
+
+plt.style.use('seaborn')
+
+# model parameter
+N = 100
+gamma =  0.25
+
+fig1, axes1 = plt.subplots(nrows=3, ncols=3, sharex=False)
+fig1.set_size_inches(18.5, 10.5)
+
+for beta, row in zip([0.03, 0.06, 0.1], range(0, 3)):
+    for h, col in zip([0.01, 0.5, 2.0], range(0, 3)):
+        # the differential equation for I 
+        I_dot = lambda t, I: beta*(N - I)*I - gamma*I
+        
+        # set parameters
+        I_initial = 10
+        a = 0
+        b = 50 * h
+        
+        # run integrator and collect data
+        t_list_euler, I_list_euler = euler_method(I_dot, I_initial, a, b, h)
+        t_list_heun, I_list_heun = heun_method(I_dot, I_initial, a, b, h)
+        
+        
+        # begin plot
+        ax = axes1[row, col]
+        ax.plot(t_list_euler, I_list_euler, 
+                label='euler method')
+        ax.plot(t_list_heun, I_list_heun,
+                label='heun method')
+        ax.set_xlabel('time')
+        ax.set_ylabel('number of infectious')
+        ax.set_title('beta = {}, h = {}'.format(beta, h), loc='right')
+        ax.legend(loc='lower right')
+
+plt.tight_layout()
+fig1.savefig('p4_figure.png')
---
+++ assignment1/code/P3_heun_method.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 11 12:08:44 2019
+
+"""
+
+
+def heun_method(y_dot, initial_condition, a, b, h):
+    """
+    y_dot: the function y_dot = f(t, y)
+    initial_condition = y(t = a)
+    [a, b]: the interval between which you want to compute y
+    h: step size
+    
+    return the simulated time series and the corresponding y values
+    """
+    # initialization
+    t = a
+    t_list = [t]
+    y = initial_condition
+    y_list = [initial_condition]
+    
+    # compute y value inductiively
+    while t < b:
+        y_pre = y + h * y_dot(t, y)
+        y = y + (h/2)*(y_dot(t, y) + y_dot(t + h, y_pre))
+        t = t + h
+        t_list.append(t)
+        y_list.append(y)
+    
+    return t_list, y_list

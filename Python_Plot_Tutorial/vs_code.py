1c51c80fe057c779c5bc5641fdf3e5ac32a748d8
LOVGC <yanzhanggc@gmail.com>
2019-09-24 21:43:23 -0400

    haha

--- Python_Plot_Tutorial/vs_code.py
+++ Python_Plot_Tutorial/vs_code.py
@@ -2,15 +2,5 @@ import math
 from os import rename
 import sys
 
-print("hello world")
-
-
-def greet():
-    test = "Test"
-    return "haha"
-
-
-greet()
-
 name = input("Your Name? ")
 print("hello", name)

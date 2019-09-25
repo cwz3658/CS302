320c479f76e31eded47df6d129c5dee089763c17
LOVGC <yanzhanggc@gmail.com>
2019-09-24 21:37:39 -0400

    pass

---
+++ Python_Plot_Tutorial/vs_code.py
@@ -0,0 +1,16 @@
+import math
+from os import rename
+import sys
+
+print("hello world")
+
+
+def greet():
+    test = "Test"
+    return "haha"
+
+
+greet()
+
+name = input("Your Name? ")
+print("hello", name)

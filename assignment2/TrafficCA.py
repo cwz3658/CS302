0e8071bedad5bad7877697c50a80bea6f335fde8
Yan <yanzhanggc@gmail.com>
2019-10-03 21:05:35 -0400

    p1 finishes

--- assignment2/TrafficCA.py
+++ assignment2/TrafficCA.py
@@ -51,15 +51,16 @@ class TrafficFlowCA:
     @classmethod
     def random_create(cls, lane_num, L, num_cars, sim_time):
         # setup a simulation
+        random.seed()
         rows = range(lane_num)
         cols = range(L)
         all_positions = list(itertools.product(rows, cols))
 
-        cars_init_speed_vec = np.random.randint(0, 3, size=num_cars)
-        cars_maxspeed_vec = np.random.randint(3, 6, size=num_cars)
+        cars_init_speed_vec = np.random.randint(0, 5, size=num_cars)
+        cars_maxspeed_vec = np.random.randint(4, 7, size=num_cars)
         cars_initial_pos_vec = random.sample(all_positions, num_cars)
-        prob_change_lane_vec = np.random.random(num_cars)
-        prob_decrease_speed_vec = np.random.random(num_cars)
+        prob_change_lane_vec = np.random.random(num_cars) * 0.5 + 0.5
+        prob_decrease_speed_vec = np.random.random(num_cars) * 0.2
 
         t_ca = cls(
             lane_num,
@@ -91,11 +92,11 @@ class TrafficFlowCA:
         fig = plt.figure()
         artists = []
         for config in self.config_history:
-            plot = plt.imshow(config, aspect="auto")
+            plot = plt.imshow(config)
             artists.append([plot])
 
         anima = ArtistAnimation(fig, artists, interval=200, repeat=False)
-        plt.show()
+        plt.show(anima)
 
     def sim_get_traffic_flow(self):
         car_list = self.car_list
@@ -109,92 +110,55 @@ class TrafficFlowCA:
         return tf_flow
 
 
-t_ca = TrafficFlowCA.random_create(10, 400, 1000, 100)
-t_ca.run_sim()
-t_ca.sim_show()
-print(t_ca.sim_get_traffic_flow())
-# def sim_driver_show(lane_num, L, num_cars, sim_time):
-#     """
-#     display a simulation
-#     """
-#     # setup a simulation
-#     rows = range(lane_num)
-#     cols = range(L)
-#     all_positions = list(itertools.product(rows, cols))
-
-#     cars_init_speed_vec = np.random.randint(0, 3, size=num_cars)
-#     cars_maxspeed_vec = np.random.randint(3, 6, size=num_cars)
-#     cars_initial_pos_vec = random.sample(all_positions, num_cars)
-#     prob_change_lane_vec = np.random.random(num_cars)
-#     prob_decrease_speed_vec = np.random.random(num_cars)
-
-#     t_ca = TrafficFlowCA(
-#         lane_num,
-#         L,
-#         num_cars,
-#         cars_init_speed_vec,
-#         cars_maxspeed_vec,
-#         cars_initial_pos_vec,
-#         prob_change_lane_vec,
-#         prob_decrease_speed_vec,
-#         sim_time,
-#     )
-
-#     t_ca.run_sim()
-#     fig = plt.figure()
-#     artists = []
-#     for config in t_ca.config_history:
-#         plot = plt.imshow(config, aspect="auto")
-#         artists.append([plot])
-
-#     anima = ArtistAnimation(fig, artists, interval=300, repeat=False)
-#     plt.show()
+def get_traffic_flow_distribution(
+    lane_num, car_density, L=1000, sim_time=600, num_trials=10
+):
+    """
+    sim_time is in seconds: sim_time = 600 means 10 min
+    L unit is 10m: L = 1000 means 1000m 
+    """
+    # compute car num
+    num_cars = round((lane_num * L) * car_density)
+    # create a t_ca
+    flows = []
+    for _ in range(num_trials):
+        t_ca = TrafficFlowCA.random_create(lane_num, L, num_cars, sim_time)
+        t_ca.run_sim()
+        flows.append(t_ca.sim_get_traffic_flow())
+    return flows
 
 
-# def get_traffic_flow(t_ca, lane_num, L, sim_time):
-#     car_list = t_ca.car_list
+lane_num_list = [2, 3, 5, 10]
+car_density_list = [0.2, 0.5, 0.7]
+fig, ax = plt.subplots()
+x_pos = range(len(lane_num_list))
 
-#     # compute total speed during this simulation
-#     total_speed = 0
-#     for car in car_list:
-#         total_speed = total_speed + np.sum(car.speed_history)
+flow_data = []
+for car_density, lane_num in itertools.product(car_density_list, lane_num_list):
+    flows = get_traffic_flow_distribution(lane_num, car_density, num_trials=10)
 
-#     tf_flow = total_speed / (sim_time * lane_num * L)
-#     return tf_flow
+    flow_data.append(flows)
+    if len(flow_data) == len(lane_num_list):
+        y_means = np.mean(flow_data, axis=1)  # compute mean along rows
+        y_std = np.std(flow_data, axis=1)
+        print("flow data is ", flow_data)
+        print("y means ", y_means)
+        print("y std", y_std)
+        ax.errorbar(
+            x_pos,
+            y_means,
+            yerr=y_std,
+            capsize=10,
+            label="Car Density = {}".format(car_density),
+        )
+        flow_data = []
 
-
-# def sim_driver_traffic_flow(lane_num, L, num_cars, sim_time):
-#     """
-#     return the traffic flow for this simulation
-#     """
-#     # setup a simulation
-#     rows = range(lane_num)
-#     cols = range(L)
-#     all_positions = list(itertools.product(rows, cols))
-
-#     cars_init_speed_vec = np.random.randint(0, 3, size=num_cars)
-#     cars_maxspeed_vec = np.random.randint(3, 6, size=num_cars)
-#     cars_initial_pos_vec = random.sample(all_positions, num_cars)
-#     prob_change_lane_vec = np.random.random(num_cars)
-#     prob_decrease_speed_vec = np.random.random(num_cars)
-
-#     t_ca = TrafficFlowCA(
-#         lane_num,
-#         L,
-#         num_cars,
-#         cars_init_speed_vec,
-#         cars_maxspeed_vec,
-#         cars_initial_pos_vec,
-#         prob_change_lane_vec,
-#         prob_decrease_speed_vec,
-#         sim_time,
-#     )
-
-#     t_ca.run_sim()
-
-#     return get_traffic_flow(t_ca, lane_num, L, sim_time)
-
-
-# print(sim_driver_traffic_flow(3, 50, 150, 100))
-
-# sim_driver_show(10, 400, 1000, 100)
+ax.set_ylabel("flow")
+ax.set_xticks(x_pos)
+x_labels = ["{} lanes".format(lane) for lane in lane_num_list]
+ax.set_xticklabels(x_labels)
+ax.set_title("Result")
+ax.yaxis.grid(True)
+plt.legend()
+plt.savefig("TrafficCA_statistics.png")
+plt.show()
---
+++ assignment2/plot_test.py
@@ -0,0 +1,39 @@
+from matplotlib import pyplot as plt
+
+# print(plt.style.available)
+# plt.style.use('fivethirtyeight')  # ggplot
+plt.xkcd()  # xkcd comics style
+
+ages_x = [1, 2, 3]
+
+dev_y = [4, 5, 6]
+plt.plot(
+    ages_x,
+    dev_y,
+    color="#5a7d9a",
+    linestyle="--",
+    marker=".",
+    linewidth=3,
+    label="All Devs",
+)  # label is used to specify legend
+
+py_dev_y = [4.4, 5.5, 6.6]
+plt.plot(ages_x, py_dev_y, label="Python")
+
+# plt.legend(['All Devs', 'Python'])  # add a list of legends for each plot in the order that they are plotted
+
+js_dev_y = [10, 20, 30]
+plt.plot(ages_x, js_dev_y, label="JavaScript")
+
+plt.xlabel("Ages")
+plt.ylabel("Median Salary")
+plt.title("Median Salary (USD) by Age")
+plt.legend()
+
+# plt.grid(True)
+plt.tight_layout()
+
+plt.savefig("plot.png")  # save the image in a png file
+plt.show()
+
+# format strings: https://matplotlib.org/3.1.1/api/_as_gen/matplotlib.pyplot.plot.html

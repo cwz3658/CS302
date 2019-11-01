d7978423eaa2c8cf0e1f62bec5058ef2ae409ff8
Wenzhe Chen <chen@ip0af5c76e.int.uvm.edu>
2019-11-01 14:03:10 -0400

    aaah

--- assignment3/P1/SRW.py
+++ assignment3/P1/SRW.py
@@ -9,61 +9,18 @@ Created on Wed Oct 23 19:48:30 2019
 import numpy as np
 import matplotlib.pyplot as plt
 
-result=[]
+def SRW(M,N,placement):
+    result=[]
+    for i in range(M):
+        z=np.random.choice([-1, 1], size=N)
+        S=np.sum(z)
+        result.append(S/np.sqrt(N))
+    fig=plt.subplot(placement)
+    fig.hist(result,bins=20)
+    fig.set_title('M=' +str(M)+ ' N=' +str(N))
 
-fig, figs = plt.subplots(2, 2, sharey=True, tight_layout=True)
-
-M=50
-N=10
-for i in range(M):
-    z=np.random.choice([-1, 1], size=N)
-    S=np.sum(z)
-    result.append(S/np.sqrt(N))    
-
-fig1=plt.subplot(221)
-fig1.hist(result,bins=20)
-fig1.set_title('M=50 N=10')
-
-result=[]
-M=50
-N=1000
-for i in range(M):
-    z=np.random.choice([-1, 1], size=N)
-    S=np.sum(z)
-    result.append(S/np.sqrt(N))    
-fig2=plt.subplot(222)
-fig2.hist(result,bins=20)
-fig2.set_title('M=50 N=1000')
-
-
-result=[]
-M=5000
-N=10
-for i in range(M):
-    z=np.random.choice([-1, 1], size=N)
-    S=np.sum(z)
-    result.append(S/np.sqrt(N))    
-fig3=plt.subplot(223)
-fig3.hist(result,bins=20)
-fig3.set_title('M=5000 N=10')
-
-
-result=[]
-M=5000
-N=1000
-for i in range(M):
-    z=np.random.choice([-1, 1], size=N)
-    S=np.sum(z)
-    result.append(S/np.sqrt(N))    
-fig4=plt.subplot(224)
-fig4.hist(result,bins=20)
-fig4.set_title('M=5000 N=1000')
-
-
-#z=np.random.choice([-1, 1], size=N)
-#S=np.cumsum(z)
-#plt.figure()
-#plt.plot(S)
-
-
-
+plt.subplots(2, 2, sharey=True, tight_layout=True)
+SRW(50,10,221) 
+SRW(50,1000,222) 
+SRW(5000,10,223) 
+SRW(5000,1000,224) 

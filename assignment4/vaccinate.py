b7f663abfca4f7d26ea9639f10883e09930adae2
cwz3658 <cwz3658@gmail.com>
2019-11-19 18:27:07 -0500

    haha

--- assignment4/vaccinate.py
+++ assignment4/vaccinate.py
@@ -23,19 +23,12 @@ for i in range(kmax):
     pk[i]=(p*(1-p)**i)
     Ik[i]=((np.random.rand(1)-0.5)*0.01+ip)*pk[i]
 
-global beta, alpha   
 # Contact rate, beta, and recovery rate, gamma, (in 1/week).
+global beta, alpha 
 beta, alpha = 0.3, 1
 
 # A grid of time points (in weeks)
 t = np.linspace(0, 160, 160)
-
-#def SIS(Ik,t):
-##    y1=lambda_I*k*(pk-Ik)*theta
-#    y1=lambda_I*Ik
-#    y2=lambda_I*Ik
-##    theta=sum(np.array(range(0,10,1))*Ik)/sum(np.array(range(0,10,1))*pk)
-#    return y1
 
 # The SIS model differential equations.
 def SIS(I, t):
@@ -44,9 +37,73 @@ def SIS(I, t):
     for k in range(kmax):
         dIk[k] = beta*k*(pk[k]-I[k])*theta-alpha*I[k]
     return dIk
+
+
+
+
 # Initial conditions vector
 I0=Ik
 # Integrate the SIR equations over the time grid, t.
-ret = odeint(SIS, I0, t)
-I=np.zeros(kmax)
-I = ret.T
+plt.figure()
+Ik = odeint(SIS, I0, t)
+lines=plt.plot(t, Ik*100)
+plt.legend(lines[:kmax], ["k={}".format(k) for k in range(kmax)])
+plt.title('SIS outbreak of nodes in each degree (no vaccination)')
+plt.xlabel ('Time (/weeks)')
+plt.ylabel ('infected fraction over entire population (%)')
+
+
+
+
+def SISV(I, t,nu, ro):
+    dIk=np.zeros(kmax)
+    theta=sum(np.array(range(0,kmax,1))*I)/sum(np.array(range(0,kmax,1))*pk)
+    for k in range(kmax):
+        dIk[k] = beta*k*(pk[k]-I[k])*theta*(nu*ro+1-nu)-alpha*I[k]
+    return dIk
+
+plt.figure()
+# vaccinated population, nu and the transmission rate reduced factor ro
+nu = 0.4
+for i in range(0,11,2):
+    ro=1-i/10
+    Ikv = odeint(SISV, I0, t, args=(nu, ro))
+    Total=sum(Ikv.T)
+    plt.plot(t, Total,label="rho={}%".format(np.int((1-ro)*100)))
+    plt.legend()
+plt.title('SIS outbreak eradicated by vaccination')
+plt.xlabel ('Time (/weeks)')
+plt.ylabel ('infected fraction over entire population (%)')
+
+
+#comparison 
+plt.figure()
+Eventual_I=[]
+nu = 0.4
+for i in range(0,110,2):
+    ro=1-i/100
+    Ikv = odeint(SISV, I0, t, args=(nu, ro))
+    Total=sum(Ikv.T)
+    Eventual_I.append(Total[159])
+Eventual_I.reverse()
+rho = np.linspace(0, 1, 55)
+plt.plot(rho, Eventual_I)
+plt.title('40% randomly vaccinated')
+plt.xlabel ('1-rho')
+plt.ylabel ('infected population when system is stable (%)')
+
+
+Eventual_I=[]
+plt.figure()
+nu = 0.8
+for i in range(0,110,2):
+    ro=1-i/100
+    Ikv = odeint(SISV, I0, t, args=(nu, ro))
+    Total=sum(Ikv.T)
+    Eventual_I.append(Total[159])
+Eventual_I.reverse() 
+rho = np.linspace(0, 1, 55)
+plt.plot(rho, Eventual_I)
+plt.title('40% population in highest degree gets vaccine')
+plt.xlabel ('1-rho')
+plt.ylabel ('infected population when system is stable (%)')

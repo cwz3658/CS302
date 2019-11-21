d1a34a096f8d726859958842aacc622f1b3315b4
cwz3658 <cwz3658@gmail.com>
2019-11-21 11:07:43 -0500

    haha

--- assignment4/voter.py
+++ assignment4/voter.py
@@ -5,31 +5,47 @@ Created on Fri Nov 15 16:56:40 2019
 @author: cwz36
 """
 
-import matplotlib
-#matplotlib.use('TkAgg')
-from pylab import random,cla,cm
-import networkx as nx
-import random as rd
 
-def initialize():
-    global g
-    g=nx.karate_club_graph()
-    g.pos=nx.spring_layout(g)
-    for i in g.nodes_iter():
-        g.nodes[i]['state']= 1 if random() < 0.5 else 0
-        
-def observe():
-    global g
-    cla
-    nx.draw(g, cmap= cm.binary, vmin =0, vmax=1,
-            node_color =[g.node[i]['state'] for i in g.nodes_iter() ], 
-            pos=g.pos)
-    
-def update():
-    global g
-    listener= rd.choice(g.nodes())
-    speaker=rd.choice(g.neighbors(listener))
-    g.node[listener]['state']=g.node[speaker]['state']
-    
-import pycxsimulator
-pycxsimulator.GUI().start(func=[initialize,observe,update])
+
+#def initialize():
+#    global g
+#    g=nx.karate_club_graph()
+#    g.pos=nx.spring_layout(g)
+#    for i in g.nodes_iter():
+#        g.nodes[i]['state']= 1 if random() < 0.5 else 0
+#        
+#def observe():
+#    global g
+#    cla
+#    nx.draw(g, cmap= cm.binary, vmin =0, vmax=1,
+#            node_color =[g.node[i]['state'] for i in g.nodes_iter() ], 
+#            pos=g.pos)
+#    
+#def update():
+#    global g
+#    listener= rd.choice(g.nodes())
+#    speaker=rd.choice(g.neighbors(listener))
+#    g.node[listener]['state']=g.node[speaker]['state']
+#    
+
+
+
+import matplotlib.pyplot as plt
+import networkx as nx
+
+G=nx.read_edgelist("airport.txt")
+
+
+options = {
+    'node_color': 'black',
+    'node_size': 50,
+    'line_color': 'grey',
+    'linewidths': 0,
+    'width': 0.1,
+}
+nx.draw(G, **options)
+plt.show()
+
+
+
+

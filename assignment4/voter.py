38ba65a6801de7a66e70c332d54b26b03883e5e6
cwz3658 <cwz3658@gmail.com>
2019-11-21 19:12:13 -0500

    23333

--- assignment4/voter.py
+++ assignment4/voter.py
@@ -1,51 +1,92 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 15 16:56:40 2019
-
-@author: cwz36
-"""
-
-
-
-#def initialize():
-#    global g
-#    g=nx.karate_club_graph()
-#    g.pos=nx.spring_layout(g)
-#    for i in g.nodes_iter():
-#        g.nodes[i]['state']= 1 if random() < 0.5 else 0
-#        
-#def observe():
-#    global g
-#    cla
-#    nx.draw(g, cmap= cm.binary, vmin =0, vmax=1,
-#            node_color =[g.node[i]['state'] for i in g.nodes_iter() ], 
-#            pos=g.pos)
-#    
-#def update():
-#    global g
-#    listener= rd.choice(g.nodes())
-#    speaker=rd.choice(g.neighbors(listener))
-#    g.node[listener]['state']=g.node[speaker]['state']
-#    
-
-
-
 import matplotlib.pyplot as plt
 import networkx as nx
+import numpy as np
 
-G=nx.read_edgelist("airport.txt")
+def initialize():
+    global G
+    G=nx.read_edgelist("airport.txt")
 
+def observe(ini):
+    global G,ones    
+    for i in G:
+        G.nodes[i]['state']= 1 if np.random.random() < ini else 0
+    ones=0
+    for i in G:
+        ones=ones+G.node[i]['state']
 
-options = {
-    'node_color': 'black',
-    'node_size': 50,
-    'line_color': 'grey',
-    'linewidths': 0,
-    'width': 0.1,
-}
-nx.draw(G, **options)
-plt.show()
+def update():
+    global G, ones
+    listener= np.random.choice(G.nodes())
+    speaker=np.random.choice(list(G.neighbors(listener)))
+    if not G.node[listener]['state']==G.node[speaker]['state']:
+        if G.node[speaker]['state']==1:
+            ones=ones+1
+        else:
+            ones=ones-1
+    G.node[listener]['state']=G.node[speaker]['state']
 
+    
+initialize()
+observe(0.5)
+ones_list=[]
+while not (ones==500 or ones==0):
+    ones_list.append(ones)
+    update()
+plt.figure()
+plt.plot(ones_list)
+plt.title('Network Voter-model')
+plt.xlabel ('Step')
+plt.ylabel ('number of red nodes')
 
+#repeat 100 times 50/50
+result=[]
+k_list=[]
+for i in range(100):
+    observe(0.5)
+    k=0
+    while not (ones==500 or ones==0):
+        k=k+1
+        update()
+    if ones==500:
+        result.append(1)
+    else:
+        result.append(0)
+    k_list.append(k)
 
+#repeat 100 times 20/80
+result2=[]
+for i in range(100):
+    observe(0.2)
+    while not (ones==500 or ones==0):
+        update()
+    if ones==500:
+        result2.append(1)
+    else:
+        result2.append(0)
+plt.figure()
+plt.hist([result,result2], bins=2,label=['50/50','20/80'])
+plt.legend()
+plt.title('majority nodes color at consensus with different initials')
+plt.xlabel('blue red')
+
+# generate configuration model
+initialize()
+degree_sequence = [d for n, d in G.degree()]
+G=nx.configuration_model(degree_sequence)
+k_list3=[]
+for i in range(100):
+    observe(0.5)
+    k=0
+    while not (ones==500 or ones==0):
+        k=k+1
+        update()
+    k_list3.append(k)
+plt.figure()
+plt.hist(k_list)
+plt.title('real network')
+plt.xlabel ('number of steps reachs consensus')
+plt.figure()
+plt.hist(k_list3)
+plt.title('configuration model network')
+plt.xlabel ('number of steps reachs consensus')
 
